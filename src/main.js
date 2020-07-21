fbf0edd3c1fe1900939d89b20ef35368525318dc
Mike1Q84 <Mike1Q84@users.noreply.github.com>
2020-07-22 00:59:07 +1000

    feat: Handle HTML DOM update with the help of the `tic-tac-toe` game from React tutorial

--- src/main.js
+++ src/main.js
@@ -1,26 +1,52 @@
 import { ToyReact, Component } from "./ToyReact"
+import "./style.css"
 
-class MyComponent extends Component {
+class Square extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: null
+    }
+  }
+
   render() {
     return (
-      <div>
-        <div>
-          <span>Hello</span>
-          <span>world</span>
-          <span>!</span>
-        </div>
-        <div>
-          {true}
-          {this.children}
-        </div>
-      </div>
-    )
+      <button className="square" onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value ? this.state.value : ""}
+      </button>
+    );
   }
 }
 
-const a = <MyComponent name="a" id="id-a">
-  <span>Child</span>
-</MyComponent>
+class Board extends Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+const a = <Board />
 
 ToyReact.render(
   a,
