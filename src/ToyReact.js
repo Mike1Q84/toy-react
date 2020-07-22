be2b93ea7b2cf0ec13c84934d3027030a8f17807
Mike1Q84 <Mike1Q84@users.noreply.github.com>
2020-07-22 23:39:29 +1000

    feat: Naive partial rendering

--- src/ToyReact.js
+++ src/ToyReact.js
@@ -53,9 +53,13 @@ class ElementWrapper {
 class TextWrapper {
   constructor(content) {
     this.root = document.createTextNode(content)
+    this.type = "#text"
+    this.chidren = []
+    this.props = Object.create(null)
   }
 
   mountTo(range) {
+    this.range = range
     range.deleteContents()
     range.insertNode(this.root)
   }
@@ -65,6 +69,10 @@ export class Component {
   constructor() {
     this.children = []
     this.props = Object.create(null)
+  }
+
+  get type() {
+    return this.constructor.name
   }
 
   setAttribute(name, value) {
@@ -79,7 +87,74 @@ export class Component {
 
   update() {
     const vdom = this.render()
-    vdom.mountTo(this.range)
+    if (this.vdom) {
+      const isSameNode = (node1, node2) => {
+        if (node1.type !== node2.type) {
+          return false
+        }
+
+        for (const name in node1.props) {
+          if (typeof node1.props[name] === "function"
+            && typeof node2.props[name] === "function"
+            && node1.props[name].toString() === node2.props[name].toString()) {
+            continue
+          }
+
+          if (typeof node1.props[name] === "object"
+            && typeof node2.props[name] === "object"
+            && JSON.stringify(node1.props[name]) === JSON.stringify(node2.props[name])) {
+            continue
+          }
+
+          if (node1.props[name] !== node2.props[name]) {
+            return false
+          }
+        }
+
+        if (Object.keys(node1.props).length !== Object.keys(node2.props).length) {
+          return false
+        }
+
+        return true
+      }
+
+      const isSameTree = (node1, node2) => {
+        if (!isSameNode(node1, node2)) {
+          return false
+        }
+
+        if (node1.children.length != node2.children.length) {
+          return false
+        }
+
+        for (let index = 0; index < node1.children.length; index++) {
+          if (!isSameTree(node1.children[index], node2.children[index])) {
+            return false
+          }
+        }
+
+        return true
+      }
+
+      const replace = (newTree, oldTree, indent) => {
+        if (isSameTree(newTree, oldTree)) {
+          return
+        }
+
+        if (!isSameNode(newTree, oldTree)) {
+          newTree.mountTo(oldTree.range)
+        } else {
+          for (let index = 0; index < newTree.children.length; index++) {
+            replace(newTree.children[index], oldTree.children[index], "  " + indent)
+          }
+        }
+      }
+
+      replace(vdom, this.vdom, "")
+    } else {
+      vdom.mountTo(this.range)
+    }
+    this.vdom = vdom
   }
 
   appendChild(vchild) {
